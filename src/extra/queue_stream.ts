eb6dca97cda20caa053c8df56d49b39c3d730671
Nickolay K <nickolay.klyuchev@tracxpoint.com>
2017-11-19 17:54:59 +0200

    adds extra entities

--- src/extra/queue_stream.ts
+++ src/extra/queue_stream.ts
@@ -5,6 +5,12 @@ export class QueueStream<T> extends Stream<T> {
     protected _queue: T[] = [];
 
     public emit(data: T): this {
+        this.append(data);
+
+        return this;
+    }
+
+    public append(data: T): this {
         this._queue.push(data);
 
         return this;
